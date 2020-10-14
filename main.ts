0c2404c5191dcdfe42891bb844732ee6651ad0c6
Jordi Simon <72779969+jordisl@users.noreply.github.com>
2020-10-14 23:25:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(392, music.beat(BeatFraction.Half))
+    encen_1()
     music.playTone(392, music.beat(BeatFraction.Half))
+    encen_1()
     music.playTone(440, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
@@ -12,6 +14,11 @@ input.onButtonPressed(Button.A, function () {
     music.playTone(587, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Double))
 })
+function encen_1 () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(50)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
 basic.forever(function () {
 	
 })
